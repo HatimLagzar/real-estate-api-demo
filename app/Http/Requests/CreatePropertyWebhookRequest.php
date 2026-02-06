adfd0c9a4320f24d5b7199c1b8f29ae9acf8cc34
Hatim <lagzarhatim@gmail.com>
2026-02-06 18:12:36 +0800

    create property webhook request validation

---
+++ app/Http/Requests/CreatePropertyWebhookRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Property;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePropertyWebhookRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            Property::USER_ID_COLUMN => ['required', 'integer'],
+            Property::PROPERTY_TYPE_COLUMN => ['required', 'string', 'in:residential,commercial,land'],
+            Property::FEATURES_COLUMN => ['nullable', 'array'],
+            Property::FEATURES_COLUMN . '.*' => ['string'],
+            Property::PRICE_COLUMN => ['nullable', 'numeric', 'min:0'],
+            Property::TAXES_COLUMN => ['nullable', 'numeric', 'min:0'],
+            Property::INCOME_COLUMN => ['nullable', 'numeric', 'min:0'],
+            Property::EXPENDITURE_COLUMN => ['nullable', 'numeric', 'min:0'],
+        ];
+    }
+}
